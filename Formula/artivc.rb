7d5093f1c6921dbbdfabe9493e9dafb022ec5046
popcorny <celu@infuseai.io>
2022-03-23 14:09:49 +0800

    Bump v0.6.0
    Signed-off-by: popcorny <celu@infuseai.io>

---
+++ Formula/artivc.rb
@@ -0,0 +1,17 @@
+class ArtiVC < Formula
+  desc "A version control system to manage large files."
+  homepage "https://github.com/InfuseAI/ArtiVC"
+  url "https://github.com/InfuseAI/ArtiVC.git",
+      tag:      "v0.6.0"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    # Don't dirty the git tree
+    rm_rf ".brew_home"
+
+    system "make", "build", "VERSION=v0.6.0"
+    bin.install "bin/avc"
+  end
+end
--- update-release.sh
+++ update-release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-TAG=$(curl -s https://api.github.com/repos/infuseai/artiv/releases |  jq -r '.[0].tag_name')
+TAG=$(curl -s https://api.github.com/repos/infuseai/artivc/releases |  jq -r '.[0].tag_name')
 echo "Tag: ${TAG}"
 
-OUT=Formula/artiv.rb
-cat ./artiv.rb.tmpl | sed "s|ARTIV_TAG|${TAG}|g" > ${OUT}
+OUT=Formula/artivc.rb
+cat ./artivc.rb.tmpl | sed "s|ARTIVC_TAG|${TAG}|g" > ${OUT}
 
 echo "Formula: "
 cat "${OUT}"
